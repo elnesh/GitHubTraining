539f6226d113053a6655f7c6eadbf144d8f752c9
elenesh <elena_nesheva@hotmail.com>
2017-12-17 16:04:15 -0800

    adding new solution

---
+++ GitHubTraining/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTraining
+{
+    public class Car
+    {
+        public string Model;
+        public int Year;
+        public string Color;
+
+        public Car (string model, int year, string color)
+        {
+            Model = model;
+            Year = year;
+            Color = color;
+        }
+
+        public Car()
+        {
+            Model = "";
+            Year = 0;
+            Color = "";
+        }
+    }
+}
---
+++ GitHubTraining/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHubTraining
+{
+    class Program
+    {
+        public static int calculateMeanYear(List<Car> cars)
+        {
+            int avarage = 0;
+            int sum = 0;
+
+            foreach (Car car in cars)
+            {
+                sum = sum + car.Year;
+            }
+
+            avarage = sum / cars.Count;
+
+            return avarage;
+        }
+
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Mazda", 2020, "Blue");
+            Car car2 = new Car("Nissan", 2010, "Red");
+
+            List<Car> cars = new List<Car> { car1, car2};
+            int avarage = calculateMeanYear(cars);
+
+            Console.WriteLine(avarage);
+        }
+    }
+}
